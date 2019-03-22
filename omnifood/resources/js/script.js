94dccb59f319a98afdcc0072ff314ef6cc5bc316
rishikeshpuri <rishikesh0296@gmail.com>
2019-03-22 16:57:29 +0530

    html5,css3

---
+++ omnifood/resources/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+    
+    $('h1').click(function() {
+        $(this).css('background-color','#ff0000')
+        
+        
+        
+    })
+    
+    
+    
+    
+    
+    
+    
+   
+});
